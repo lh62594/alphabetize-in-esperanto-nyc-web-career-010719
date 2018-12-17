19920f035e9e598b4fd68972ab50634064448eec
breezy-karma-0220 <lh62594@yahoo.com>
2018-12-17 20:26:59 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,8 +3,15 @@ require 'pry'
 def alphabetize(arr)
   # code here
 
-  esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esp_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
   #use .index method to get the index of the letter?
+
+  arr.sort_by do |word|
+    word.split(" ").collect do |letter|
+      esp_alphabet.index(letter)
+    end
+  end
+
 
 
 end
