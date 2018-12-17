5dc3ecb482a4e414f390ae27f4cb08db6d63b6db
breezy-karma-0220 <lh62594@yahoo.com>
2018-12-17 20:30:29 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -7,9 +7,8 @@ def alphabetize(arr)
 
   arr.sort_by do |word|
     word.split('').collect do |letter|
-      esperanto_alphabet.index(letter)
+      esp_alphabet.index(letter)
     end
   end
-end
 
 end
