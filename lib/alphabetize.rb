86b6b5d72d84c0c066f6cdab0bf2f6c5074c5cf8
breezy-karma-0220 <lh62594@yahoo.com>
2018-12-17 20:25:25 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -6,10 +6,13 @@ def alphabetize(arr)
   esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   #use .index method to get the index of the letter?
 
-  arr.sort_by do |word|
-    word.split(" ").map do |letter|
-      binding.pry 
-      esp_alphabet.index(letter)
+  arr.sort_by do |phrase|
+    phrase.split(" ").map do |word|
+      word.split(" ").map do |letter|
+      binding.pry
+
+        esp_alphabet.index(letter)
+      end
     end
   end
 
