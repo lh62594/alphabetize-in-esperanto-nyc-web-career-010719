a6ad219f21bc6a83347a8ed2f1b611e4389992cb
breezy-karma-0220 <lh62594@yahoo.com>
2018-12-17 20:30:41 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 def alphabetize(arr)
   # code here
+
+  esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  arr.sort_by do |word|
+    word.split('').collect do |letter|
+      esp_alphabet.index(letter)
+    end
+  end
+
 end
